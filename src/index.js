8fa8138c2ce7cbdfd02c09bc1519c48aa78c5ad9
YuliyaKasyanchuk <yuljakas@gmail.com>
2016-10-08 23:21:59 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@
 var car = require('js/car.js');
 
 console.log(car)
+car.drive(100);
+console.log(car.speed);
+car.put();
+car.put();
+car.put();
+car.put();
+
+console.log(car.passengers);
+car.land();
+car.land();
+car.land();
+
+console.log(car.passengers);
