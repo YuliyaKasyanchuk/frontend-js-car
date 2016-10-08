f58ad33c407b512b5e4b9fb7df59007812bba3c0
YuliyaKasyanchuk <yuljakas@gmail.com>
2016-10-08 23:22:13 +0300

    Add files via upload

--- src/js/car.js
+++ src/js/car.js
@@ -6,16 +6,22 @@
     this.color = "white"
     this.dours = 4 
     this.speed = 0
-    this.defaultSpeed= 60
-    this.maxSpeed=100
-    this.seats= 4
-    this.passengers=[]
+    this.defaultSpeed = 60
+    this.maxSpeed = 100
+    this.seats = 4
+    this.passengers = []
    
     this.drive = function(speed){ 
-        if(speed>=this.speed && speed<= this.maxSpeed){
+        if((speed >= this.speed && speed <= this.maxSpeed) && (this.passengers.length !== 0)){
             this.speed = speed 
         }
-        else if(typeof speed!=='number'){
+        else if ((speed >= this.speed && speed <= this.maxSpeed) && (this.passengers.length === 0)){
+            this.speed = 0
+        }
+         else if (this.passengers.length === 0){
+            this.speed = 0
+        }
+        else if(typeof speed !== 'number'){
             this.speed = this.defaultSpeed 
         }
         else{
@@ -23,39 +29,26 @@
         }
     }
    
-    this.put=function(){
-        if(this.passengers.length <= this.seats-1){
+    this.put = function(){
+        if(this.passengers.length <= this.seats - 1){
              this.passengers.push(true);
          }
         else{
-            throw new Error('can\'t be put more than seats')
+            this.passengers.length = this.seats;
         }
     
      }
 
-     this.land=function(){
-        if(this.passengers.length>0){
+     this.land = function(){
+        if(this.passengers.length > 0){
              this.passengers.shift(true);
          } 
          else{
-            throw new Error('No passengers left')
+           this.speed = 0;
         }
      }
 }
-var car=new Car();
-module.exports=car;
-var car=new Car();
-car.drive(100);
-console.log(car.speed);
-car.put();
-car.put();
-car.put();
-car.put();
-
-console.log(car.passengers);
-car.land();
-car.land();
-car.land();
-
-console.log(car.passengers);
+var car = new Car();
+module.exports = car;
+var car = new Car();
 
